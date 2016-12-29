11d649c430209441336be0285abba60c32cad149
zyf <zhaoyafei0210@gmail.com>
2016-12-29 19:29:46 +0800

    add HelloDemo/*

---
+++ HelloDemo/Demo/demo.cxx
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "hello.h"
+#include "demo_b.h"
+
+//void hello();
+//void demo_b_hello();
+
+void main()
+{
+    printf("Call hello():\n");
+    hello();
+	printf("Call demo_b_hello():\n");
+	demo_b_hello();
+}
---
+++ HelloDemo/Demo/demo_b.cxx
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void demo_b_hello()
+{
+    printf("Hello from demo_b.cxx!\n");
+}
